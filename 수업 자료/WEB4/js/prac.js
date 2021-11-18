a854b5762532c3b391b35d995438c4e7a512e52f
SangWook-H <90087727+SangWook-H@users.noreply.github.com>
2021-11-18 22:30:44 +0900

    d
    d

---
+++ 수업 자료/WEB4/js/prac.js
@@ -0,0 +1,9 @@
+var heading = document.querySelector(".heading");
+
+window.onkeydown = function(){
+    heading.style.fontSize = "100px";
+}
+
+window.onkeyup = function(){
+    heading.style.fontSize = "32px";
+}
